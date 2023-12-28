4ee8f0609d3ff35b1a55b822a821349fa42cf3aa
TJChiang <tjchiang8419@gmail.com>
2023-12-28 17:28:50 +0800

    主程式啟動

--- main.go
+++ main.go
@@ -1,15 +1,27 @@
 package main
 
 import (
+	libBrowser "bahamut/internal/browser"
 	"bahamut/internal/config"
 	"bahamut/pkg/login"
 )
 
 func main() {
-	v, err := config.InitConfig()
+	c, err := config.InitConfig()
 	if err != nil {
 		panic(err)
 	}
 
-	login.Login(v)
+	browser, context, err := libBrowser.Launch(c.GetBrowserConfig())
+	if err != nil {
+		panic(err)
+	}
+	defer browser.Close()
+	defer context.Close()
+	page, err := context.NewPage()
+	if err != nil {
+		panic(err)
+	}
+	defer page.Close()
+	login.Login(c.GetModulesConfig().Login, &page)
 }
